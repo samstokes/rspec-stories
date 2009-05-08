ce3efc2d48ba54bf6f8dcc072bf4bc5a45075e5c
Sam Stokes <sam@mo.jo>
2009-05-08 13:44:34 +0100

    avoid NoMethodError on failing scenario (caused by RSpec change maybe?)

--- lib/spec/runner/formatter/story/plain_text_formatter.rb
+++ lib/spec/runner/formatter/story/plain_text_formatter.rb
@@ -77,7 +77,7 @@ module Spec
           end
         
           def scenario_failed(story_title, scenario_name, err)
-            @options.backtrace_tweaker.tweak_backtrace(err)
+            @options.backtrace_tweaker.tweak_backtrace(err) if @options.backtrace_tweaker
             @failed_scenarios << [story_title, scenario_name, err] unless @scenario_already_failed
             @scenario_already_failed = true
             @story_failed = true
